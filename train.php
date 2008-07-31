acd11dde3440c8d2a515575fd63a8467dcf79619
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-07-31 15:30:57 +0200

    added short description to train script

--- train.php
+++ train.php
@@ -1,4 +1,12 @@
 <?php
+/**
+* Example script that shows how to train the guesser based on 
+* raw text in a language.
+* 
+* Analazyes the content of files in ./train/new and creates
+* ".train" files usuable by the guesser.
+*/
+
 require_once('Guess.php');
 $guess = new Guess(array('modeldir' => './train/new'));
 
